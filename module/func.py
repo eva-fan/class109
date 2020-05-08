c050ab6c1619a07efb81b84abc2647f00133cbc3
eva-fan <62133588+eva-fan@users.noreply.github.com>
2020-05-08 14:39:44 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -95,23 +95,23 @@ def sendQuickreply(event):  #快速選單
 def sendButton(event):  #按鈕樣版
     try:
         message = TemplateSendMessage(
-            alt_text='按鈕樣板',
+            alt_text='有什麼好玩的嗎？',
             template=ButtonsTemplate(
-                thumbnail_image_url='https://i.imgur.com/4QfKuz1.png',  #顯示的圖片
+                thumbnail_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7022Cmi0lfveFaq108SEmW-YYoW-LF0cYBIRpmgUrVkP3wPjf&usqp=CAU',  #顯示的圖片
                 title='按鈕樣版示範',  #主標題
-                text='請選擇：',  #副標題
+                text='你要：',  #副標題
                 actions=[
                     MessageTemplateAction(  #顯示文字計息
-                        label='文字訊息',
-                        text='@購買披薩'
+                        label='摸摸牠',
+                        text='@摸摸牠'
                     ),
                     URITemplateAction(  #開啟網頁
-                        label='連結網頁',
-                        uri='http://www.e-happy.com.tw'
+                        label='看看牠',
+                        uri='https://reurl.cc/Aq64dp'
                     ),
                     PostbackTemplateAction(  #執行Postback功能,觸發Postback事件
-                        label='回傳訊息',  #按鈕文字
-                        #text='@購買披薩',  #顯示文字計息
+                        label='稱讚牠',  #按鈕文字
+                        text='@稱讚牠',  #顯示文字計息
                         data='action=buy'  #Postback資料
                     ),
                 ]
